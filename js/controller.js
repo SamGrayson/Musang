d3c1f35db372cf2738c3286185057e93e23745e1
Sam Grayson <samuel.c.grayson17@gmail.com>
2015-07-06 13:49:04 -0400

    gh-pages test

--- js/controller.js
+++ js/controller.js
@@ -3,25 +3,26 @@
   angular
     .module('musang')
     .controller('MainController', function($scope, ShowsService, $location, $routeParams){
-      $scope.inputPlaceholder = 'Enter an artist';
+      $scope.inputPlaceholder = 'Enter an artist'
 
       $scope.artist = {
-        name: 'ACDC'
       }
 
-      ShowsService.read($scope.artist.name).success(function(shows) {
-        $scope.shows = shows;
-        console.log(shows)
-      });
-
+      $scope.shows = []
 
       // ShowsService.readOne($routeParams.id).succes(function(show) {
       //   $scope.show = show;
       // })
 
-      $scope.clickEvent = function(obj) {
-        console.log($scope.artist.name);
-        $location.path('/artist' + '/' + $scope.artist.name);
+      $scope.clickEvent = function(artist, $event) {
+        $event.stopPropagation();
+        $event.preventDefault();
+        var promise = ShowsService.read(artist.name);
+        promise.success(function(shows) {
+          $scope.shows = shows
+          console.log($scope.shows);
+          $location.path('/artist' + '/' + artist.name)
+        });
       }
 
     });
